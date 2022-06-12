ede84c302c185737bcdaa259239d135fad5276c7
David Vicente <davidvicento@gmail.com>
2022-06-12 15:04:31 +0200

    Add methods tools to class Bot

--- veepeebot/bots/Bot.py
+++ veepeebot/bots/Bot.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 class Bot:
     def __init__(self, hide):
@@ -13,14 +14,33 @@ class Bot:
         # Open browser
         self.driver = webdriver.Chrome(options=options)
     
-    def shutdown(self):
-        self.driver.close()
+    
+    # METHODS
+    
+    def get(self, xpath):
+        return self.driver.find_element(By.XPATH, xpath)
+
+    def getContent(self, xpath):
+        return self.get(xpath).get_attribute('innerHTML')
+
+    def exist(self, xpath):
+        try:
+            self.driver.find_element(By.XPATH, xpath)
+            return True
+        except NoSuchElementException:
+            return False
+
+
+    # ACTIONS
 
     def go(self, url):
         self.driver.get(url)
 
     def click(self, xpath):
-        self.driver.find_element(By.XPATH, xpath).click() 
+        self.get(xpath).click() 
 
     def write(self, xpath, text):
-        self.driver.find_element(By.XPATH, xpath).send_keys(text)
+        self.get(xpath).send_keys(text)
+
+    def shutdown(self):
+        self.driver.close()
